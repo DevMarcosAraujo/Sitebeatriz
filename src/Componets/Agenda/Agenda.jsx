88a06a7046c1b2ba92277fd3f40fc15f30e30f37
MarcosAraujo <marcosbrasilbr@gmail.com>
2025-04-22 14:23:35 -0300

    atualizando whatsapp

--- src/Componets/Agenda/Agenda.jsx
+++ src/Componets/Agenda/Agenda.jsx
@@ -1,20 +1,24 @@
 import "./Agenda.css"
 
 function Agenda () {
+    const whatsappNumber = "5561981353643"; // Substitua pelo número real
+    const message = "Olá, gostaria de agendar uma consulta.";
+    const whatsappLink = `https://wa.me/${whatsappNumber}?text=${encodeURIComponent(message)}`;
+
     return(
         <main className="cards">
-        <div className="card azul">
-          <h3>AGENDAR</h3>
-          <p>Serviços de consulta e outras categorias</p>
-        </div>
-        <div className="card verde">
-          <h3>MEUS AGENDAMENTOS</h3>
-          <p>Consulte ou cancele seus agendamentos</p>
-        </div>
-       
-      </main>
-    
+            <a href={whatsappLink} target="_blank" rel="noopener noreferrer" className="card-link">
+                <div className="card azul">
+                    <h3>AGENDAR</h3>
+                    <p>Serviços de consulta e outras categorias</p>
+                </div>
+            </a>
+            <div className="card verde">
+                <h3>MEUS AGENDAMENTOS</h3>
+                <p>Consulte ou cancele seus agendamentos</p>
+            </div>
+        </main>
     )
 }
 
-export default Agenda 
+export default Agenda;
--- src/Pages/Inicio.jsx
+++ src/Pages/Inicio.jsx
@@ -1,3 +1,4 @@
+import React from "react";
 import "../App.css";
 import imagemBeatriz1 from '../assets/CONVITE-00237.jpg';
 
@@ -9,28 +10,21 @@ import Agenda from "../Componets/Agenda/Agenda";
 function Inicio() {
   return (
     <>
-    <Banner />
-  
+      <Banner />
       <div className="pagina-inicio">
         <div className="texto">
-          
           <h1>Bem-vindo <br /> Sou a Doutora Beatriz Vilela</h1>
           <p>
             Cuidar de você é minha prioridade. Aqui, promovemos o bem-estar físico, mental e emocional com dedicação,
             empatia e excelência. Descubra um novo jeito de viver com mais saúde e equilíbrio.
           </p>
-          
         </div>
         <div className="imagem">
           <img src={imagemBeatriz1} alt="Doutora Beatriz" />
         </div>
       </div>
+     
       <Agenda />
-      
-
-      
-      
-  
     </>
   );
 }
